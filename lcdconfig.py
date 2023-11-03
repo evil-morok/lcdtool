5b23cda1b058cb80a5e66635ffe0cc9c8db75a92
rogov ak <alexrogov.0x07b7@gmail.com>
2023-11-03 18:24:10 +0700

    color transition

--- lcdconfig.py
+++ lcdconfig.py
@@ -2,12 +2,34 @@ import display
 import random
 from time import sleep
 
-End = False
+TransitionTime = 2000
+
+rgb = [0, 0, 0]
+newRGB = [0, 0, 0]
+deltas = [0, 0, 0]
 
 def every1ms(context):
-    sleep(2)
-    display.backlight(random.randint(0, 0xffffff))
+    global rgb, newRGB, deltas, TransitionTime
+    
+    if abs(rgb[0] - newRGB[0]) < 1 and abs(rgb[1] - newRGB[1]) < 1 and abs(rgb[2] - newRGB[2]) < 1:
+        newRGB = [
+            random.randint(0, 255), 
+            random.randint(0, 255), 
+            random.randint(0, 255)
+        ]
+        deltas = [
+            (newRGB[0] - rgb[0]) / TransitionTime, 
+            (newRGB[1] - rgb[1]) / TransitionTime, 
+            (newRGB[2] - rgb[2]) / TransitionTime
+        ]
 
+    rgb[0] += deltas[0]
+    rgb[1] += deltas[1]
+    rgb[2] += deltas[2]
+
+    color = (int(rgb[0]) & 0xff) + ((int(rgb[1]) & 0xff) << 8) + ((int(rgb[2]) & 0xff) << 16)
+
+    display.backlight(color)
 
 def onStart(context):
     print("=============INIT!===================")
